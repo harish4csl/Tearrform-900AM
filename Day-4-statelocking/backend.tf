2538518524acc8e5c3c4807025f0d738a4f28ab0
hari <hari4cs@gmail.com>
2025-08-09 16:34:44 +0530

    Day-4-backed.tf inside statefile lock is added

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "terrformcowork-bucket"
     key    = "Day-4/statelocking/teerrform.tfstate"
     region = "ap-south-1"
+    use_lockfile = true #s3 supports this feature but teraaform version > 1.10, latest version >=1.10
   }
 }
